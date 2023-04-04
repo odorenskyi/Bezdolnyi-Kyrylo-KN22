68277baebe7cf90532c75270a21ecea4f127241e
Gubochka <newicraft2201@gmail.com>
2023-04-04 17:14:44 +0300

    Виправлено заголовковий файл

--- lab09/prj/ModulesBesdolny/ModulesBezdolny.h
+++ lab09/prj/ModulesBesdolny/ModulesBezdolny.h
@@ -1,6 +1,8 @@
 #ifndef MODULESBEZDOLNY_H_INCLUDED
 #define MODULESBEZDOLNY_H_INCLUDED
 
+using namespace std;
+
 float s_calculation(float x, float y, float z);
 string bofort(float wind);
 double celsius_to_fahrenheit(double temp_C);
