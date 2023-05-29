7e1e54808efbe9c15da6b2fa773643a956ac65c7
Gubochka <newicraft2201@gmail.com>
2023-05-29 19:30:22 +0300

    11 лабораторна робота виконана TEAM O.Shchychenko, D.Pashchenko, K.Bezdolnyi

---
+++ lab11/prj/.h-files/ModulesBezdolnyi.h
@@ -0,0 +1,27 @@
+#ifndef MODULESBEZDOLNY_H_INCLUDED
+#define MODULESBEZDOLNY_H_INCLUDED
+
+#include <vector>
+#include <struct_type_project_6.h>
+using namespace std;
+
+float s_calculation(float x, float y, float z);
+string bofort(float wind);
+double celsius_to_fahrenheit(double temp_C);
+double average_temperature_C(const double temp_C[], int days);
+string binary_count(unsigned int iNumber);
+
+void abilityToEdit(string outputFileName, string inputFileName);
+bool fillInputTxtFile(string inputFileName, string ukrWord);
+bool authorInfo(string outputFileName);
+int vowelsCountInFile(string outputFileName, string ukrWord);
+bool findWordInPoem(string outputFileName, string ukrWord);
+int consonantsCountInFile(string inputFileName, string ukrWord);
+bool timestampInFile(string inputFileName);
+double sCalculationResInFile(string outputFileName, float x, float y, float z);
+bool decimalToBinary(string outputFileName, unsigned int number);
+
+void getRecordUseBusStation(vector<BusSchedule>& schedule);
+void getAllInfo(vector<BusSchedule>& schedule);
+
+#endif // MODULESBEZDOLNY_H_INCLUDED
